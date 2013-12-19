c064e3713bd03290974b6f976c5f2826ed99944f
Michael Toomim <toomim@cs.washington.edu>
2013-12-18 19:13:32 -0800

    added partial help for first_hit bonus feature

--- web2py/applications/utility/controllers/utiliscope.py
+++ web2py/applications/utility/controllers/utiliscope.py
@@ -129,6 +129,11 @@ def hit():
 def submit():
     redirect(turk_submit_url())
 
+def submit_first_hit():
+    soft_assert(db((db.actions.workerid == request.workerid)
+                   & (db.actions.action == 'finished')).count() < 1)
+    hit_finished(bonus_amount=request.first_time_bonus or 0.51)
+ 
 
 def hello():
     theme = 'whitepink'
